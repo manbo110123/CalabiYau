413b0a5a195243fffedb21e795721348b02fda11
manbo110123 <15029484004@163.com>
2026-08-02 00:37:51 +0800

    修复

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852a2bcf961984aacd05a191c97f025869f65b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f3fcfcf715ef6cfb75c945d6ffbc5f97e4e972")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
